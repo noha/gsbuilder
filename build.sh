590eca837a331d70fd97154f2395142c2faac4e5
Norbert Hartl <norbert@hartl.name>
2011-05-16 15:09:23 +0200

    draft copy of hudson build script for gemstone

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+DIR=`dirname $0`
+SCRIPTS_PATH="$DIR/scripts"
+STONE_CREATOR=/opt/stone-creator
+FRESH_EXTENT=""
+
+#SCRIPTS=("$SCRIPTS_PATH/before.st")
+SCRIPTS=()
+
+while getopts ":i:n:s:f?" OPT ; do
+        case "$OPT" in
+
+        s) if [ -f "$SCRIPTS_PATH/$OPTARG.st" ] ; then
+              SCRIPTS=("${SCRIPTS[@]}" "$SCRIPTS_PATH/$OPTARG.st")
+           else
+              echo "$(basename $0): invalid script ($OPTARG)"
+              exit 1
+           fi
+        ;;
+	f)
+            FRESH_EXTENT="-f"
+ 	;;
+	n)
+            BUILD_NAME="$OPTARG$BUILD_NUMBER"
+ 	;;
+   esac
+done
+
+SCRIPTS=("${SCRIPTS[@]}" "$SCRIPTS_PATH/after.st")
+
+$STONE_CREATOR/create-stone.sh -n $BUILD_NAME -d $WORKSPACE $FRESH_EXTENT -s 200000 -t 500000 -u jenkins
+. $WORKSPACE/env
+
+rm $WORKSPACE/script.st
+
+eval "echo \"$(< $SCRIPTS_PATH/before.st )\"" > $WORKSPACE/$BUILD_NAME-script.st
+
+for FILE in "${SCRIPTS[@]}" ; do
+	echo $FILE
+	cat $FILE >> $WORKSPACE/$BUILD_NAME-script.st
+	#eval "echo \"$(< $FILE )\"" >> $WORKSPACE/$BUILD_NAME-script.st
+done 
+
+startstone $BUILD_NAME -z $GEMSTONE_SYS_CONF -l $WORKSPACE/log.txt
+
+$GEMSTONE/bin/topaz -l -T200000 < $WORKSPACE/$BUILD_NAME-script.st
+STATE=$?
+echo "state: $STATE"
+
+stopstone $BUILD_NAME DataCurator swordfish
+
+exit $STATE
